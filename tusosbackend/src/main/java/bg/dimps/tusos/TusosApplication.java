7b4c4cffc0aa4f3e66ce54cd7b1f28e3fd0e9fb4
bloommyy <35131685+bloommyy@users.noreply.github.com>
2022-11-16 19:17:39 +0200

    Initial commit

---
+++ tusosbackend/src/main/java/bg/dimps/tusos/TusosApplication.java
@@ -0,0 +1,13 @@
+package bg.dimps.tusos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TusosApplication.class, args);
+	}
+
+}
---
+++ tusosbackend/src/test/java/bg/dimps/tusos/TusosApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.dimps.tusos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
