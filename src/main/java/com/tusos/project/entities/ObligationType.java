d99898765baa60be6d2f81692055f36805aa24e0
Savina Valchanova <75179050+savina01@users.noreply.github.com>
2022-11-23 14:14:09 +0200

    entities

---
+++ src/main/java/com/tusos/project/entities/ObligationType.java
@@ -0,0 +1,40 @@
+package com.tusos.project.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ObligationType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer type;
+    private String name;
+
+    public ObligationType() {
+    }
+
+    public ObligationType(Integer type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
