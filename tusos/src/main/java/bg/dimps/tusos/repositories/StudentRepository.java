b9338264717b2c1ea8d66f2bf8c942f4a5a4c7e6
Savina Valchanova <vera0555@abv.bg>
2022-12-01 17:32:25 +0200

    Fixing StudentController

--- tusos/src/main/java/bg/dimps/tusos/repositories/StudentRepository.java
+++ tusos/src/main/java/bg/dimps/tusos/repositories/StudentRepository.java
@@ -1,7 +1,12 @@
 package bg.dimps.tusos.repositories;
 
+import bg.dimps.tusos.entities.MonetaryObligation;
 import bg.dimps.tusos.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findByFacultyNumber(String facultyNumber);
+
 }
