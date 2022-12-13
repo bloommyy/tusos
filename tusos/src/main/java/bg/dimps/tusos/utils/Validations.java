9a205a8f48ba26ea5cb8a10e4924d7a5b881242b
bloommyy <35131685+bloommyy@users.noreply.github.com>
2022-12-14 00:40:12 +0200

    Switched Student to User, moved it to utils folder
    Also formatted file

---
+++ tusos/src/main/java/bg/dimps/tusos/utils/Validations.java
@@ -0,0 +1,40 @@
+package bg.dimps.tusos.utils;
+
+import bg.dimps.tusos.entities.User;
+
+public class Validations {
+    User user;
+
+    private boolean validateEmail(String email) {
+        return email.matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$");
+    }
+
+    private boolean validatePassword(String password) {
+        return password.length() >= 8;
+    }
+
+    private boolean validateRepeatedPassword(String repeatedPassword) {
+        return repeatedPassword.equals(user.getPassword());
+    }
+
+    private boolean validateFirstName(String firstName) {
+        return firstName.length() > 1 && firstName.matches("[A-Z]\\w*") && firstName.matches("[a-z]");
+    }
+
+    private boolean validateMiddleName(String middleName) {
+        return middleName.length() > 1 && middleName.matches("[A-Z]\\w*") && middleName.matches("[a-z]");
+    }
+
+    private boolean validateLastName(String lastName) {
+        return lastName.length() > 1 && lastName.matches("[A-Z]\\w*") && lastName.matches("[a-z]");
+    }
+
+    private boolean validateFacultyNumber(String facultyNumber) {
+        return facultyNumber.length() == 9 && facultyNumber.matches("[0-9]");
+    }
+
+    private boolean validatePhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("^\\+(?:[0-9] ?){6,14}[0-9]$");
+    }
+
+}
